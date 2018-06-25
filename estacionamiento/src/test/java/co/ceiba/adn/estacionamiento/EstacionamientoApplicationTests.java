f51cd797d65806c7b895af8e9c37d53aa56d891d
CEIBA\julian.rivera <julian.rivera@ceiba.com.co>
2018-06-25 17:16:18 -0500

    Se cambia el runner de las pruebas

--- estacionamiento/src/test/java/co/ceiba/adn/estacionamiento/EstacionamientoApplicationTests.java
+++ estacionamiento/src/test/java/co/ceiba/adn/estacionamiento/EstacionamientoApplicationTests.java
@@ -1,13 +1,7 @@
 package co.ceiba.adn.estacionamiento;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(MockitoJUnitRunner.class)
-@SpringBootTest
 public class EstacionamientoApplicationTests {
 
 	@Test
